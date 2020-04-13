f2cb74128ff659f1639c13de75df83804ebcfd6c
AdiRadhMapf <63603218+AdiRadhMapf@users.noreply.github.com>
2020-04-13 17:41:26 +0100

    Initial Commit

---
+++ GameMakerCore/GameMakerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameMakerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
